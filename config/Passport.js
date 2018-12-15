2429ceea481a4ec0d4b1092b76425125f278e4cf
NilakashSarkar <nilakashsarkar2@gmail.com>
2018-12-15 15:51:11 +0530

    make it better

--- config/Passport.js
+++ config/Passport.js
@@ -1,6 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const  ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose=require('mongoose')
+const User = mongoose.model('users');
 const keys=require('./keys')
 var opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
